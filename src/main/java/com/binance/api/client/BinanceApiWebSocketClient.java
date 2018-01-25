750115f79ec6208b7e948e4e9ae0a9420b54e62f
Andy Coates <andy.coates@apple.com>
2018-01-25 11:26:50 +0000

    Improvements to error handling and the ability to shutdown the client and/or a active web socket. - Optional `onFailure` added to `BinanceApiCallback` to allow user to opt-in to receiving failure info. - `BinanceApiWebSocketClient` enhanced to return `Closeable`s from calls creating web sockets, so that said web sockets can be later closed, if needed. - `BinanceApiWebSocketClient` enhanced to be `Closeable` itself, closing the internal OKHttp dispatcher. - `BinanceApiWebSocketListener` no longer throws an exception from `onFailure`, as this was just being thrown up the stack to the `Thread.uncaughtExceptionHandler`.
    With these changes I can now:
    - detect failures on the web socket and choose to do something about them.
    - shutdown the `BinanceApiWebSocketClient`, and potentially recreate later, without any resource leaks or references being held to my callback objects.

--- src/main/java/com/binance/api/client/BinanceApiWebSocketClient.java
+++ src/main/java/com/binance/api/client/BinanceApiWebSocketClient.java
@@ -1,25 +1,25 @@
 package com.binance.api.client;
 
-import java.util.List;
 import com.binance.api.client.domain.event.AggTradeEvent;
-import com.binance.api.client.domain.event.AllMarketTickersEvent;
 import com.binance.api.client.domain.event.CandlestickEvent;
 import com.binance.api.client.domain.event.DepthEvent;
 import com.binance.api.client.domain.event.UserDataUpdateEvent;
 import com.binance.api.client.domain.market.CandlestickInterval;
 
+import java.io.Closeable;
+
 /**
  * Binance API data streaming façade, supporting streaming of events through web sockets.
  */
-public interface BinanceApiWebSocketClient {
+public interface BinanceApiWebSocketClient extends Closeable {
 
-  void onDepthEvent(String symbol, BinanceApiCallback<DepthEvent> callback);
+  Closeable onDepthEvent(String symbol, BinanceApiCallback<DepthEvent> callback);
 
-  void onCandlestickEvent(String symbol, CandlestickInterval interval, BinanceApiCallback<CandlestickEvent> callback);
+  Closeable onCandlestickEvent(String symbol, CandlestickInterval interval, BinanceApiCallback<CandlestickEvent> callback);
 
-  void onAggTradeEvent(String symbol, BinanceApiCallback<AggTradeEvent> callback);
+  Closeable onAggTradeEvent(String symbol, BinanceApiCallback<AggTradeEvent> callback);
 
-  void onUserDataUpdateEvent(String listenKey, BinanceApiCallback<UserDataUpdateEvent> callback);
+  Closeable onUserDataUpdateEvent(String listenKey, BinanceApiCallback<UserDataUpdateEvent> callback);
 
-  void onAllMarketTickersEvent(BinanceApiCallback<List<AllMarketTickersEvent>> callback);
+  void close();
 }
--- src/main/java/com/binance/api/client/impl/BinanceApiWebSocketClientImpl.java
+++ src/main/java/com/binance/api/client/impl/BinanceApiWebSocketClientImpl.java
@@ -1,20 +1,18 @@
 package com.binance.api.client.impl;
 
-import java.io.Closeable;
-import java.io.IOException;
-import java.util.List;
-import okhttp3.Dispatcher;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import com.binance.api.client.BinanceApiCallback;
 import com.binance.api.client.BinanceApiWebSocketClient;
 import com.binance.api.client.constant.BinanceApiConstants;
 import com.binance.api.client.domain.event.AggTradeEvent;
-import com.binance.api.client.domain.event.AllMarketTickersEvent;
 import com.binance.api.client.domain.event.CandlestickEvent;
 import com.binance.api.client.domain.event.DepthEvent;
 import com.binance.api.client.domain.event.UserDataUpdateEvent;
 import com.binance.api.client.domain.market.CandlestickInterval;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.WebSocket;
+
+import java.io.Closeable;
 
 /**
  * Binance API WebSocket client implementation using OkHttp.
@@ -24,45 +22,43 @@ public class BinanceApiWebSocketClientImpl implements BinanceApiWebSocketClient,
   private OkHttpClient client;
 
   public BinanceApiWebSocketClientImpl() {
-    Dispatcher d = new Dispatcher();
-    d.setMaxRequestsPerHost(100);
-    this.client = new OkHttpClient.Builder().dispatcher(d).build();
+    this.client = new OkHttpClient();
   }
 
-  public void onDepthEvent(String symbol, BinanceApiCallback<DepthEvent> callback) {
+  public Closeable onDepthEvent(String symbol, BinanceApiCallback<DepthEvent> callback) {
     final String channel = String.format("%s@depth", symbol);
-    createNewWebSocket(channel, new BinanceApiWebSocketListener<>(callback, DepthEvent.class));
+    return createNewWebSocket(channel, new BinanceApiWebSocketListener<>(callback, DepthEvent.class));
   }
 
   @Override
-  public void onCandlestickEvent(String symbol, CandlestickInterval interval, BinanceApiCallback<CandlestickEvent> callback) {
+  public Closeable onCandlestickEvent(String symbol, CandlestickInterval interval, BinanceApiCallback<CandlestickEvent> callback) {
     final String channel = String.format("%s@kline_%s", symbol, interval.getIntervalId());
-    createNewWebSocket(channel, new BinanceApiWebSocketListener<>(callback, CandlestickEvent.class));
+    return createNewWebSocket(channel, new BinanceApiWebSocketListener<>(callback, CandlestickEvent.class));
   }
 
-  public void onAggTradeEvent(String symbol, BinanceApiCallback<AggTradeEvent> callback) {
+  public Closeable onAggTradeEvent(String symbol, BinanceApiCallback<AggTradeEvent> callback) {
     final String channel = String.format("%s@aggTrade", symbol);
-    createNewWebSocket(channel, new BinanceApiWebSocketListener<>(callback, AggTradeEvent.class));
+    return createNewWebSocket(channel, new BinanceApiWebSocketListener<>(callback, AggTradeEvent.class));
   }
 
-  public void onUserDataUpdateEvent(String listenKey, BinanceApiCallback<UserDataUpdateEvent> callback) {
-    createNewWebSocket(listenKey, new BinanceApiWebSocketListener<>(callback, UserDataUpdateEvent.class));
+  public Closeable onUserDataUpdateEvent(String listenKey, BinanceApiCallback<UserDataUpdateEvent> callback) {
+    return createNewWebSocket(listenKey, new BinanceApiWebSocketListener<>(callback, UserDataUpdateEvent.class));
   }
 
-  public void onAllMarketTickersEvent(BinanceApiCallback<List<AllMarketTickersEvent>> callback) {
-    final String channel = "!ticker@arr";
-    createNewWebSocket(channel, new BinanceApiWebSocketListener<List<AllMarketTickersEvent>>(callback));
-  }
-
-  private void createNewWebSocket(String channel, BinanceApiWebSocketListener<?> listener) {
+  private Closeable createNewWebSocket(String channel, BinanceApiWebSocketListener<?> listener) {
     String streamingUrl = String.format("%s/%s", BinanceApiConstants.WS_API_BASE_URL, channel);
     Request request = new Request.Builder().url(streamingUrl).build();
-    client.newWebSocket(request, listener);
+    final WebSocket webSocket = client.newWebSocket(request, listener);
+    return () -> {
+      final int code = 1000;
+      listener.onClosing(webSocket, code, null);
+      webSocket.close(code, null);
+      listener.onClosed(webSocket, code, null);
+    };
   }
 
   @Override
-  public void close() throws IOException {
+  public void close() {
     client.dispatcher().executorService().shutdown();
   }
-
 }
